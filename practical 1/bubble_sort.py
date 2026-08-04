43b73d8e1e2e720e320cf68fb89018659fb77a0f
nageshreddymandala938-star <nageshreddymandala938@gmail.com>
2026-08-04 11:27:10 +0530

    Update bubble_sort.py

--- practical 1/bubble_sort.py
+++ practical 1/bubble_sort.py
@@ -1,1 +1,16 @@
+def bubble_sort(arr):
+    n = len(arr)
 
+    for i in range(n):
+        for j in range(0, n - i - 1):
+            if arr[j] > arr[j + 1]:
+                arr[j], arr[j + 1] = arr[j + 1], arr[j]
+
+# Get user input
+numbers = list(map(int, input("Enter numbers separated by spaces: ").split()))
+
+print("Original list:", numbers)
+
+bubble_sort(numbers)
+
+print("Sorted list:", numbers)
