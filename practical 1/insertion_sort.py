37708a9783a060b6509890a7e7a125238eaea01b
nageshreddymandala938-star <nageshreddymandala938@gmail.com>
2026-08-04 11:28:16 +0530

    Add insertion sort function with user input
    Implemented insertion sort algorithm and added user input for sorting a list of numbers.

--- practical 1/insertion_sort.py
+++ practical 1/insertion_sort.py
@@ -1,1 +1,22 @@
+def insertion_sort(arr):
+    n = len(arr)
 
+    for i in range(1, n):
+        key = arr[i]
+        j = i - 1
+
+        # Move elements greater than key one position ahead
+        while j >= 0 and arr[j] > key:
+            arr[j + 1] = arr[j]
+            j -= 1
+
+        arr[j + 1] = key
+
+# Get user input
+numbers = list(map(int, input("Enter numbers separated by spaces: ").split()))
+
+print("Original list:", numbers)
+
+insertion_sort(numbers)
+
+print("Sorted list:", numbers)
